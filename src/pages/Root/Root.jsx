f9263f857f5fbee71a2d8f91bda62aedc5bad7bb
Anupam2570632 <128147498+Anupam2570632@users.noreply.github.com>
2024-04-25 23:17:03 +0600

    login and register page created

--- src/pages/Root/Root.jsx
+++ src/pages/Root/Root.jsx
@@ -1,6 +1,9 @@
 import { HelmetProvider } from "react-helmet-async";
 import { Outlet } from "react-router-dom";
 import Header from "../../components/Header";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Root = () => {
     return (
@@ -9,6 +12,7 @@ const Root = () => {
                 <Header/>
                 <Outlet />
             </HelmetProvider>
+            <ToastContainer/>
         </div>
     );
 };
